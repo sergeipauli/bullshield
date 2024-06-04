7929d1bde1c0dcafb1627395419dbd2b8caf6896
Sergei Politov <sergeipltv@yandex.ru>
2024-06-04 13:21:41 +0700

    04062024

---
+++ training/amcp/acmp_simple.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void task_0001() {
+	int a, b;
+    
+    std::cin >> a >> b;
+    
+    std::cout << a + b;
+}
+ 
+void task_0033() {
+    int a, b;
+     
+    std::cin >> a >> b;
+     
+    std::cout << b - 1 << ' ' << a - 1;
+     
+    return 0;
+}
+
+int main() {
+	int a, b;
+    
+    std::cin >> a >> b;
+    
+    std::cout << b - 1 << a - 1;
+    
+	return 0;
+}
